ca3af95e116bb72f7ab842aaff4230aaeced45d4
Guilherme Abreu <78891520+guilhermeabreu23@users.noreply.github.com>
2021-11-22 02:05:00 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,62 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Antônio Araújo',
+                partido: 'PFB',
+                fotos: [{ url: 'perfil1.jpg', legenda: 'Vereador', small: false }]
+            },
+            {
+                numero: '27123',
+                nome: 'Carla Rodrigues',
+                partido: 'PZU',
+                fotos: [{ url: 'perfil2.jpg', legenda: 'Vereadora', small: false }]
+            },
+            {
+                numero: '42789',
+                nome: 'Paulo Cerqueira',
+                partido: 'PRW',
+                fotos: [{ url: 'perfil3.jpg', legenda: 'Vereador', small: false }]
+            },
+            {
+                numero: '50188',
+                nome: 'Marcos Albuquerque',
+                partido: 'PPM',
+                fotos: [{ url: 'perfil4.jpg', legenda: 'Vereador', small: false }]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'Wesley Bragança',
+                partido: 'PFB',
+                fotos: [{ url: 'perfil5.jpg', legenda: 'Prefeito', small: false }]
+            },
+            {
+                numero: '42',
+                nome: 'Mariana Alencar',
+                partido: 'PRW',
+                fotos: [{ url: 'perfil6.jpg', legenda: 'Prefeita', small: false }]
+            },
+            {
+                numero: '27',
+                nome: 'Alexandre Cruz',
+                partido: 'PZU',
+                fotos: [{ url: 'perfil7.jpg', legenda: 'Prefeito', small: false }]
+            },
+            {
+                numero: '50',
+                nome: 'Beatriz Andrade',
+                partido: 'PPM',
+                fotos: [{ url: 'perfil8.jpg', legenda: 'Prefeita', small: false }]
+            }
+        ]
+    }
+]
